0ce7962a1f745e778b0b7c8e8f0e8bb5c897b32e
Tom Hsu <thomaschsu@gmail.com>
2019-10-18 00:14:12 -0400

    Modified redux-101 and destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,77 @@
+// // const person = {
+// //     name: 'Tom',
+// //     age: 30,
+// //     location: {
+// //         city: 'Holmdel',
+// //         temp: 55
+// //     }
+// // };
+// // const { name: firstName = 'Anonymous', age } = person
+
+// // console.log(`${firstName} is ${age}.`)
+
+// // const { city, temp: temperature } = person.location;
+
+// // if (city && temperature) {
+// //     console.log(`It's ${temperature} in ${city}`)
+// // }
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+
+// const { name: publisherName = "Self-Published" } = book.publisher;
+
+// console.log(publisherName); // Penguin, default value of: Self-Published
+
+
+// const address = [];
+
+// const [ , , state = "New Jersey" ] = address;
+
+// console.log(`You are in ${state}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [ detail, , medium ] = item;
+
+// grab first and third items using array destructuring
+console.log(`A medium ${detail} costs ${medium}`);
---
+++ src/playground/redux-101.js
@@ -0,0 +1,60 @@
+import { createStore } from 'redux';
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+// setCount
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count
+})
+const resetCount = () => ({
+    type: 'RESET'
+})
+
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            }
+        case 'DECREMENT': 
+            return {
+                count: state.count - action.decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state
+    }
+})
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
+
+
+// increment, decrement, reset
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// })
+store.dispatch(incrementCount({ incrementBy: 5}))
+store.dispatch(incrementCount())
+store.dispatch(resetCount())
+store.dispatch(decrementCount())
+store.dispatch(decrementCount({ decrementBy: 10 }))
+store.dispatch(setCount({ count: -100 }))
