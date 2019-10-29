ae002f65110ac1fb33007deb6c2fecfe8d64a51b
Tom Hsu <thomaschsu@gmail.com>
2019-10-28 22:20:30 -0400

    Fixed react-redux and fixed old packages

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -11,15 +12,26 @@ import './styles/styles.scss'
 const store = configureStore()
 
 // addExpense -> water bill
-store.dispatch(addExpense({ description: 'Water bill'}))
+store.dispatch(addExpense({ description: 'Water bill' }))
 // addExpenses -> Gas bill
-store.dispatch(addExpense({ description: 'Gas bill'}))
+store.dispatch(addExpense({ description: 'Gas bill' }))
 // setTextFilter -> bill (2 items) -> water (1 item)
 store.dispatch(setTextFilter('water'))
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('soda'))
+}, 3000)
+
 // getVisibleExpenses -> print visible ones to screen
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 console.log(store.getState())
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
